3f7548ccaf12a90e8769c9958432b80b951c313f
Anna Moskalchuk <anna@crystax.net>
2019-04-07 17:17:43 +0300

    feat: modified webpack.config.js fix: path to background-image  12 #  13 # Untracked files:

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = {
   entry: [
@@ -43,7 +44,25 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css',
       chunkFilename: '[id].css'
-    })
+    }),
+    new CopyWebpackPlugin([
+      /*      {*/
+      //from: './src/fonts',
+      //to: './fonts'
+      //},
+      //{
+      //from: './src/favicon',
+      //to: './favicon'
+      /*},*/
+      {
+        from: './src/images',
+        to: './images'
+      }/*,*/
+      //{
+      //from: './src/uploads',
+      //to: './uploads'
+      /*}*/
+    ])
   ]
 };
 
